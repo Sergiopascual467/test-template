aaf377da5b91218c53815be8825a8735d030c976
Sergiopascual467 <spascualtri@alumni.unav.es>
2024-11-21 20:50:40 +0100

    background changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/background.jpeg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Abstract composition with soft, flowing lines and wave-like patterns in shades of blue and white. It features circular elements and a clean, modern aesthetic, evoking a sense of calmness and minimalism.";
 
 const Home = ({ name, title }) => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/background.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Abstract composition with soft, flowing lines and wave-like patterns in shades of blue and white. It features circular elements and a clean, modern aesthetic, evoking a sense of calmness and minimalism.";
 
 /**
  * Project list
